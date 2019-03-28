56e9d0d33d9f04e973cdbae35b9a8e76d0f61ac1
Rafael Hernández Larios <rafah.larios@gmail.com>
2019-03-28 11:33:49 -0600

    :gift: add staff and concierges support on userHandler

--- src/userHandler.js
+++ src/userHandler.js
@@ -1,65 +1,51 @@
-//ver 0.3
-
 'use strict';
+
 const AWS = require('aws-sdk'),
     env = process.env;
 
 AWS.config.update({
     region: env.REGION
 });
-//services
+
 const cognito = new AWS.CognitoIdentityServiceProvider(),
     dynamodbDocumentClient = new AWS.DynamoDB.DocumentClient();
 
-var not_found_error = {
-    "message": "Resource not found",
-    "code": "ValidationException",
-    "statusCode": 404
+async function getUserFromJWT(AccessToken) {
+    try {
+        let user = await cognito.getUser({
+            AccessToken
+        }).promise();
+        const sub = user["UserAttributes"][0].Value;
+        user = await findUserInDB(sub);
+        return user;
+    } catch (err) {
+        throw (err);
+    }
 };
 
-var user = module.exports = {};
+async function findUserInDB(sub) {
+    try {
+        const concierge = await finUserInConciergeUsers(sub);
+        if (concierge.Count > 0) return concierge.Items[0];
+        const admin = await finUserInAdminUsers(sub);
+        if (admin.Count > 0) return admin.Items[0];
+        const co_staff = await finUserInCoStaffUsers(sub);
+        if (co_staff.Count > 0) return co_staff.Items[0];
+        const staff = await finUserInStaffUsers(sub);
+        if (staff.Count > 0) return staff.Items[0];
 
-user.getUserFromJWT = function (AccessToken, fields = []) {
-    return new Promise((resolve, reject) => {
-        let locale;
-        let user;
-        this.getCognitoUser(AccessToken)
-            .then((data) => {
-                let sub = data["UserAttributes"][0].Value;
-                return this.getUserFromTable(sub, fields);
-            })
-            .then((data) => {
-                user = data;
-                return this.getUserRelations(data);
-            })
-            .then((data) => {
-                user.hotels = data;
-                resolve(user);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
+        if (concierge.Count === 0 &&
+            admin.Count === 0 &&
+            co_staff.Count === 0 &&
+            staff.Count === 0) throw 'User does not exist';
+
+    } catch (err) {
+        throw (err);
+    }
 };
 
-
-user.getCognitoUser = function (AccessToken) {
-    return new Promise((resolve, reject) => {
-        cognito.getUser({
-                AccessToken: AccessToken
-            })
-            .promise()
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
-};
-
-user.getUserFromTable = function (uuid, fields = []) {
-    return new Promise((resolve, reject) => {
+async function finUserInAdminUsers(uuid, fields = []) {
+    try {
         let params = {
             TableName: env.DBB_ADMIN_USERS_TABLE,
             IndexName: 'uuid-index',
@@ -71,84 +57,116 @@ user.getUserFromTable = function (uuid, fields = []) {
                 '#uuid': 'uuid'
             }
         };
+        console.log('params: ', params);
         if (fields.length > 0) {
-            params = pushParamstoObject(fields, params);
+            params = await pushParamstoObject(fields, params);
         }
-        dynamodbDocumentClient.query(params).promise()
-            .then((data) => {
-                if (data.Count > 0) resolve(data.Items[0]);
-                else reject(not_found_error);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
-};
-
-user.getUserRelations = function (user) {
-    return new Promise((resolve, reject) => {
-        let params = {
-            TableName: env.DDB_HOTELS_USERS,
-            ProjectionExpression: 'hotel_uuid',
-            IndexName: 'company_uuid-index',
-            KeyConditionExpression: 'company_uuid = :company_uuid',
-            FilterExpression: 'user_uuid = :user_uuid',
-            ExpressionAttributeValues: {
-                ':company_uuid': user.company_uuid,
-                ':user_uuid': user.uuid
-            }
-        }
-        dynamodbDocumentClient.query(params).promise().then((data) => {
-            let response = [];
-            if (data.Count > 0) {
-                data.Items.forEach((hotel) => {
-                    response.push(hotel.hotel_uuid);
-                });
-            }
-            resolve(response);
-        }).catch((err) => {
-            reject(err);
-        });
-    });
-}
-
-user.cognitoUserAsJson = function (AccessToken) {
-    return new Promise((resolve, reject) => {
-        let cognitoUser = {};
-        cognito.getUser({
-                AccessToken: AccessToken
-            })
-            .promise()
-            .then((user) => {
-                user.UserAttributes.forEach((attribute) => {
-                    cognitoUser[attribute.Name] = attribute.Value;
-                });
-                resolve(cognitoUser);
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
-};
-
-function objectIsEmpty(obj) {
-    for (var key in obj) {
-        if (obj.hasOwnProperty(key))
-            return false;
+        const res = await dynamodbDocumentClient.query(params).promise();
+        return res;
+    } catch (err) {
+        throw (err);
     }
-    return true;
+};
+
+async function finUserInStaffUsers(sub) {
+    try {
+        const params = {
+            TableName: env.DDB_STAFF_USERS_TABLE,
+            KeyConditionExpression: '#uuid = :uuid',
+            ExpressionAttributeNames: {
+                '#uuid': 'uuid'
+            },
+            ExpressionAttributeValues: {
+                ':uuid': sub
+            }
+        };
+        return await dynamodbDocumentClient.query(params).promise();
+    } catch (err) {
+        throw (err);
+    }
+};
+
+async function finUserInCoStaffUsers(sub) {
+    try {
+        const params = {
+            TableName: env.DDB_CO_STAFF_USERS_TABLE,
+            KeyConditionExpression: '#uuid = :uuid',
+            IndexName: 'uuid-index',
+            ExpressionAttributeNames: {
+                '#uuid': 'uuid'
+            },
+            ExpressionAttributeValues: {
+                ':uuid': sub
+            }
+        };
+        return await dynamodbDocumentClient.query(params).promise();
+    } catch (err) {
+        throw (err);
+    }
+};
+
+async function finUserInConciergeUsers(sub) {
+    try {
+        const params = {
+            TableName: env.DDB_HOTEL_CONCIERGES,
+            KeyConditionExpression: '#uuid = :uuid',
+            IndexName: 'uuid-index',
+            ExpressionAttributeNames: {
+                '#uuid': 'uuid'
+            },
+            ExpressionAttributeValues: {
+                ':uuid': sub
+            }
+        };
+        return await dynamodbDocumentClient.query(params).promise();
+    } catch (err) {
+        throw (err);
+    }
+};
+
+async function getCognitoUser(AccessToken) {
+    try {
+        return await cognito.getUser({
+            AccessToken
+        }).promise();
+    } catch (err) {
+        throw (err);
+    }
+};
+
+async function pushParamstoObject(arr, object) {
+    try {
+        let params = object;
+        if (!params.ProjectionExpression) params.ProjectionExpression = '';
+        if (!params.ExpressionAttributeNames) params.ExpressionAttributeNames = {};
+        arr.forEach((value, index) => {
+            params.ProjectionExpression += `#${value}`;
+            params.ProjectionExpression += index < arr.length - 1 ? ',' : '';
+            params['ExpressionAttributeNames'][`#${value}`] = value;
+        });
+        return params;
+    } catch (err) {
+        throw (err);
+    }
 }
 
-function pushParamstoObject(arr, object) {
-    let params = object;
-    if (!params.ProjectionExpression) params.ProjectionExpression = '';
-    if (!params.ExpressionAttributeNames) params.ExpressionAttributeNames = {};
-    arr.forEach((value, index) => {
-        params.ProjectionExpression += `#${value}`;
-        params.ProjectionExpression += index < arr.length - 1 ? ',' : '';
-        params['ExpressionAttributeNames'][`#${value}`] = value;
-    });
-    return params;
+async function cognitoAttributesToJson(arr) {
+    try {
+        let obj = {}
+        for (const item of arr) obj[item.Name] = item.Value
+        return obj
+    } catch (err) {
+        throw (err);
+    }
 }
 
-//ToDo add async await
+module.exports = {
+    getUserFromJWT,
+    findUserInDB,
+    getCognitoUser,
+    finUserInAdminUsers,
+    finUserInStaffUsers,
+    finUserInCoStaffUsers,
+    finUserInConciergeUsers,
+    cognitoAttributesToJson
+}
